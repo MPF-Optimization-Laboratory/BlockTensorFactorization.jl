97fcc19785c539e7b92f428dbf8f03b70b5e2c16
github-action-benchmark <github@users.noreply.github.com>
2025-11-19 04:57:51 +0000

    add Benchmark (julia) benchmark result for a3de7f2eb66fe7d92cec9589daaa28088304cd1f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1763528271108,
+  "repoUrl": "https://github.com/MPF-Optimization-Laboratory/BlockTensorFactorization.jl",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "njericha@math.ubc.ca",
+            "name": "Nicholas Richardson",
+            "username": "njericha"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3de7f2eb66fe7d92cec9589daaa28088304cd1f",
+          "message": "Force benchmark after docs",
+          "timestamp": "2025-11-18T20:52:48-08:00",
+          "tree_id": "0da380e366fd285ce3f5ffb417c35fe3f2c0f132",
+          "url": "https://github.com/MPF-Optimization-Laboratory/BlockTensorFactorization.jl/commit/a3de7f2eb66fe7d92cec9589daaa28088304cd1f"
+        },
+        "date": 1763528269412,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "factorize/1",
+            "value": 16442519,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=10040808\nallocs=113709\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
