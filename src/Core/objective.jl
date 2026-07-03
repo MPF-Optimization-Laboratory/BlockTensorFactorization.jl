9cf9b3ef09afbba8a00ca3460a89730042d35459
njericha <njericha@math.ubc.ca>
2026-07-03 15:58:03 -0700

    make Lp objective

--- src/Core/objective.jl
+++ src/Core/objective.jl
@@ -30,3 +30,22 @@ Calculates the least squares objective at tensors `X` and `Y`.
 (objective::L2)(X, Y) = norm2(X - Y)
 
 # TODO Should this be 0.5norm2(X - Y) instead?
+
+"""
+    Lp <: AbstractObjective
+
+The p-norm distance objective ||X-Y||_p^p.
+"""
+struct Lp <: AbstractObjective 
+    p::Real
+end
+
+"""
+    (objective::Lp)(X, Y)
+
+Calculates the p-norm objective at tensors `X` and `Y`.
+"""
+function (objective::Lp)(X, Y)
+    p = Lp.p
+    return norm(X - Y; p)^p # TODO should this be 1/p ? I think not since p could be Inf
+end
